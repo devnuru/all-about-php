0e50ae029d2fcc6c2f3ab1e43180708cffd917f0
Md Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2021-12-08 17:35:34 +0600

    test

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ echo $y;
 
 //PHP also stores all global variables in an array called $GLOBALS[index]. The index holds the name of the variable. This array is also accessible from within functions and can be used to upadte global variables directly.
 //Ex-
-
+hello_global();
 
 
 
