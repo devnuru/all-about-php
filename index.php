e06e5834b4ad924132e17001bce93bebea4035c7
Md Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2022-01-10 00:51:08 +0600

    Basic PHP

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
-<!-- 
-PHp is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.
+<!--
+PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.
 
 PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.  -->
 
 <!-- PHP => PHP: Hypertext Preprocessor
 It is powerful enough to be at the core of the biggest blogging system on the web(WordPress)
 
-It is deep enough to run large social networks! 
+It is deep enough to run large social networks!
 
 It is also easy enough to be beginner's first server side language!  -->
 
 
 <!-- What can PHP Do?
- # PHP can generate dynamic page content 
- #PHP can create, open, read, write, delete, and close files on the server 
- # PHP can collect form data 
- # PHP can send and receive cookies  
- # PHP can add, delete, modify data in your database 
- # PHP can be used to control user-access 
+ # PHP can generate dynamic page content
+ #PHP can create, open, read, write, delete, and close files on the server
+ # PHP can collect form data
+ # PHP can send and receive cookies
+ # PHP can add, delete, modify data in your database
+ # PHP can be used to control user-access
  # PHP can encrypt data  -->
 
  <!-- ***Programming is a Profession That Requires Constant Learning
- 
+
  ***Good Developers Don't Need Anyone's help
  ***Programming Isn't The Only Thing That They're Good at!
  ***You Must Be Familiar With Your Tools
@@ -37,7 +37,7 @@ It is also easy enough to be beginner's first server side language!  -->
   #various platforms
   #compatible with almost all servers(Apache, IIS etc)
   #supports a wide range of databases
-  # free 
+  # free
   #easy to learn  -->
 
 
@@ -85,8 +85,8 @@ echo "Hi, $name. Im $age years old! ";
   #local
   #global
   #static
-
 // Global Scope
+
 $x = 25;
 function myFunc(){
     echo "<p>Variable x inside function is: $x </p>";
@@ -117,7 +117,172 @@ echo $y;
 
 //PHP also stores all global variables in an array called $GLOBALS[index]. The index holds the name of the variable. This array is also accessible from within functions and can be used to upadte global variables directly.
 //Ex-
-hello_global();
+
+
+
+// With php, there are two basic ways to get output: echo & print.
+
+
+// echo and print are more less the same. They are both used to output data to the screen.
+
+# echo has no return value while print has a return value of 1 so it can be used in expressions. echo can take multiple parameters(although such usage is rare) while print take one argument. echo is marginally faster than print.
+
+// echo and print statement can be used with or without parentheses
+
+
+/*
+
+
+
+PHP data types
+Variables can store data of different types, and different data types can do different things.
+
+PHP supports the following data types:
+
+1.String
+2.Integer
+3.Float(floating point numbers - also called double)
+4. Boolean
+5. Array
+6. Object
+7. Null
+8. Resource
+
+*/
+
+
+// Rules for integers:
+//   * An integer must have at least one digit
+//   * An integer must not have a decimal point
+//   * An integer can be either positive or negative
+//   * Integer can be specified in: decimal, hexadecimal, octal or binary notation
+
+
+// PHP var_damp() function returns the data types and value:
+
+echo "<br>";
+
+$i = 1997;
+
+var_dump($i);
+
+
+// PHP float(floating point number) is a number with a decimal point or a number in exponential form.
+
+$f = 16.10;
+
+var_dump($f);
+
+// A Boolean represents two possible states: TRUE or FALSE;
+// Boolean are often used in conditional testing.
+
+
+// An array stores multiple values in one single variable.
+
+echo "<hr>";
+
+
+$man = array("Nuruzzaman", "Khokon", "Nusrat Jahan");
+var_dump($man);
+
+
+
+
+//PHP Object
+
+//Class and objects are the two man aspects of object-oriented programming.
+
+//A class is a template for objects, and an object is an instance of a class.
+//When the individual objects are created, they inherit all the properties and behaviors from the class, but each object will have different values for the properties.
+
+//If we create a __construct() function, PHP will automatically call this function when we create an object from a class.
+
+echo "<br>";
+class Car {
+  public $color;
+  public $model;
+  public function __construct($color, $model) {
+      $this ->color =$color;
+      $this ->model =$model;
+  }
+  public function message() {
+      return "My car is a " . $this ->color . " " . $this->model . "!";
+  }
+}
+
+$myCar = new Car("Black", "Volvo");
+echo $myCar ->message();
+echo "<br>";
+
+$myCar = new Car("red", "Toyota");
+echo $myCar -> message();
+
+//PHP null value
+//Null is a special data type which can have only one value: NULL.
+
+//A variable of data tpe NULL is a variable that has no value assigned to it.
+// Tips: If a variable is created without a value, it is automatically assigned a value of NULL.
+//
+echo "<br>";
+$n = NULL;
+var_dump($n);
+
+echo "<br>";
+$u = "Hello PHP";
+$u = NULL;
+var_dump($u);
+//PHP Resource
+//The special resource type is not an actual data type. It is the storing of a reference to functions and resources external to PHP.
+//A common example of using the resource data type is a database call.
+
+//In this chapter we will look at some commonly used functions to manpulate strings.
+
+# strlen() Return the length of a string
+
+echo strlen("hello world");
+
+//str_word_count() - Count Words in a String
+echo str_word_count("He llo PHP");
+// strrev() - Reverse a String
+echo strrev("Hello Nuruzzaman");
+///strpos() - Search For a Text within a String
+
+echo "<br>";
+
+echo strpos("Hello World", "World");
+ echo "<br>";
+//Tip: The first character position in a string is 0 (not 1).
+
+//str_replace() - Replace Text Within a String
+echo str_replace("JavaScript", "PHP", "Hello JavaScript");
+
+
+strlen("Hello");
+str_word_count("Hello");
+strrev("Hello"); //6
+str_replace("world", "Nuruzzaman", "Hello World") 
+
+
+//PHP Numbers
+//One thing to notice about PHP is that it provides automatic data type conversion
+// So, if we assign an integer value to a variable, the type of that variable will automatically be an integer.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
