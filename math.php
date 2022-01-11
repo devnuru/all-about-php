0bd88f402d22da53ecf13d6d83678e4e7ea0d6be
Md Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2022-01-11 23:51:29 +0600

    PHP math added

---
+++ math.php
@@ -0,0 +1,26 @@
+<?php
+
+ //PHP has a set of math functions that allows you to perform mathematical tasks on numbers. 
+
+ //PHP pi() Function
+//The pi() function returns the value of PI: 
+
+PHP min() and max() Functions
+
+The min() and max() functions can be used to find the lowest or highest value in a list of arguments:
+
+PHP abs() Function
+The abs() function returns the absolute (positive) value of a number:
+
+
+PHP sqrt() Function
+The sqrt() function returns the square root of a number:
+
+PHP round() Function
+The round() function rounds a floating-point number to its nearest integer:
+
+Random Numbers
+The rand() function generates a random number:
+
+
+To get more control over the random number, you can add the optional min and max parameters to specify the lowest integer and the highest integer to be returned.
